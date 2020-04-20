ac62897d67ac5c3f0eb78b51fb6a4b0bf6a7cef0
EDonnelly24 <61127769+EDonnelly24@users.noreply.github.com>
2020-04-19 22:16:24 -0700

    Implementation 2 code
    This the code :)

---
+++ lib/widgets/post_tile.dart
@@ -0,0 +1,31 @@
+import 'package:collegeapp/pages/post_screen.dart';
+import 'package:collegeapp/widgets/custom_image.dart';
+import 'package:collegeapp/widgets/post.dart';
+import 'package:flutter/material.dart';
+
+class PostTile extends StatelessWidget {
+  final Post post;
+
+  PostTile(this.post);
+
+  showPost(context) {
+    Navigator.push(
+      context, 
+      MaterialPageRoute(
+        builder: (context) => PostScreen(
+          postId: post.postId,
+          userId: post.ownerId,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => showPost(context),
+      child: cachedNetworkImage
+      (post.mediaUrl),
+    );
+  }
+}
